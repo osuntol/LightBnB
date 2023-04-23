435dde3f78a39b078acaedf6707be11866ed6fc2
Rotimi Osuntola Jr <rosuntola@gmail.com>
2023-04-23 21:11:16 +0000

    Database refactored

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -108,7 +108,7 @@ const getAllReservations = function(guest_id, limit = 10) {
 const getAllProperties = function(options, limit = 5) {
   const queryParams = [];
 
-let queryString = `SELECT properties.*, avg(property_reviews.rating) as average_rating
+  let queryString = `SELECT properties.*, avg(property_reviews.rating) as average_rating
 FROM properties
 JOIN property_reviews ON properties.id = property_id
 `;
@@ -141,10 +141,20 @@ JOIN property_reviews ON properties.id = property_id
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+console.log('PROPERTY', property)
+  return pool
+    .query(
+      `INSERT INTO properties(owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms)
+  VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+  RETURNING *;`,
+      [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.street, property.city, property.province, property.post_code,
+      property.country, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms])
+    .then(res => res.rows[0])
+    .catch((err) => {
+      return null
+    })
+
+
 };
 
 module.exports = {
@@ -153,5 +163,7 @@ module.exports = {
   addUser,
   getAllReservations,
   getAllProperties,
-  addProperty,
-};
+  addProperty
+}
+
+//exports.addProperty = addProperty;
