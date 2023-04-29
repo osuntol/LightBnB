6b0a8732749aa6a8e3c2c1cf73d8ba66357cb44c
Rotimi Osuntola Jr <rosuntola@gmail.com>
2023-04-29 18:54:35 +0000

    database with canges made to functions that were not functioning

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -1,4 +1,6 @@
 const { Pool } = require('pg');
+const properties = require("./json/properties.json");
+const users = require("./json/users.json");
 
 const pool = new Pool({
   user: 'vagrant',
@@ -11,8 +13,6 @@ pool.query(`SELECT title FROM properties LIMIT 10;`)
   .then(response => { console.log(response) })
 
 
-const properties = require("./json/properties.json");
-const users = require("./json/users.json");
 
 /// Users
 
@@ -86,11 +86,11 @@ const getAllReservations = function(guest_id, limit = 10) {
   FROM reservations
   JOIN properties ON reservations.property_id = properties.id
   JOIN property_reviews ON properties.id = property_reviews.property_id
-  WHERE reservations.guest_id = 1
+  WHERE reservations.guest_id = $2
   GROUP BY properties.id, reservations.id
   ORDER BY reservations.start_date
   LIMIT $1;
-  `, [limit])
+  `, [limit, guest_id])
     .then((result) => {
       return result.rows
     })
@@ -105,23 +105,27 @@ const getAllReservations = function(guest_id, limit = 10) {
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-const getAllProperties = function(options, limit = 5) {
+const getAllProperties = function(options, limit = 20) {
+  //created empty array to push the object value from options
   const queryParams = [];
 
   let queryString = `SELECT properties.*, avg(property_reviews.rating) as average_rating
 FROM properties
 JOIN property_reviews ON properties.id = property_id
 `;
-
-  if (options.owner_id) {
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    //concatenate the query string with WHERE clause with what has been pushed into the  queryParams array
+    queryString += `WHERE city LIKE $${queryParams.length} `;
+  } if (options.owner_id) {
     queryParams.push(`${options.owner_id}`);
     queryString += `WHERE owner_id = $${queryParams.length}\n`;
   } else if (options.minimum_price_per_night && options.maximun_price_per_night) {
     queryParams.push(options.minimum_price_per_night * 100, option.maximum_price_per_night * 100)
     queryString += `WHERE cost_per_night >= $${queryParams.length - 1} AND cost_per_night <= $${queryParams.length} `
   } else if (options.minimum_rating) {
-    queryParams.push(option.minimum_rating)
-    queryString += `WHERE minimum_rating = $${queryParams.length}`
+    queryParams.push(options.minimum_rating)
+    queryString += `WHERE rating = $${queryParams.length}`
   }
 
   queryParams.push(limit);
@@ -130,8 +134,6 @@ JOIN property_reviews ON properties.id = property_id
   ORDER BY cost_per_night
   LIMIT $${queryParams.length};
   `;
-
-  console.log(queryString, queryParams);
   return pool.query(queryString, queryParams).then((res) => res.rows);
 };
 
@@ -141,13 +143,12 @@ JOIN property_reviews ON properties.id = property_id
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-console.log('PROPERTY', property)
   return pool
     .query(
       `INSERT INTO properties(owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms)
   VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
   RETURNING *;`,
-      [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.street, property.city, property.province, property.post_code,
+      [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night * 100, property.street, property.city, property.province, property.post_code,
       property.country, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms])
     .then(res => res.rows[0])
     .catch((err) => {
@@ -156,6 +157,7 @@ console.log('PROPERTY', property)
 
 
 };
+
 
 module.exports = {
   getUserWithEmail,
@@ -166,4 +168,3 @@ module.exports = {
   addProperty
 }
 
-//exports.addProperty = addProperty;
