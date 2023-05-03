611a523190bcc6a9785539f8dd909559e9f8f122
Rotimi Osuntola Jr <rosuntola@gmail.com>
2023-05-03 19:40:44 +0000

    get all properties function refactored

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -112,25 +112,62 @@ const getAllProperties = function(options, limit = 10) {
   let queryString = `SELECT properties.*, avg(property_reviews.rating) as average_rating
 FROM properties
 JOIN property_reviews ON properties.id = property_id
-GROUP BY properties.id
 `;
+
   if (options.city) {
     queryParams.push(`%${options.city}%`);
     //concatenate the query string with WHERE clause with what has been pushed into the  queryParams array
-    queryString += `WHERE city LIKE $${queryParams.length} `;
+    if (queryString.includes('WHERE')) {
+      queryString += ` 
+     AND city LIKE $${queryParams.length} 
+    `;
+    } else {
+      queryString += `WHERE city LIKE $${queryParams.length}`
+    }
+
   } if (options.owner_id) {
     queryParams.push(`${options.owner_id}`);
-    queryString += `WHERE owner_id = $${queryParams.length}\n`;
-  } else if (options.minimum_price_per_night && options.maximum_price_per_night) {
-    queryParams.push(options.minimum_price_per_night * 100, options.maximum_price_per_night * 100)
-    queryString += `WHERE cost_per_night >= $${queryParams.length - 1} AND cost_per_night <= $${queryParams.length} `
-  } else if (options.minimum_rating) {
-    queryParams.push(options.minimum_rating)
-    queryString += `HAVING avg(rating) >= $${queryParams.length}`
+    if (queryString.includes('WHERE')) {
+      queryString += `
+     AND owner_id = $${queryParams.length} 
+      `;
+    } else {
+      queryString += `WHERE owner_id = $${queryParams.length}`
+    }
+  } if (options.minimum_price_per_night) {
+    queryParams.push(options.minimum_price_per_night * 100)
+    if (queryString.includes('WHERE')) {
+      queryString += `
+     AND cost_per_night >= $${queryParams.length} 
+    `;
+    } else {
+      queryString += `WHERE cost_per_night >= $${queryParams.length}`
+    }
   }
+  if (options.maximum_price_per_night) {
+    queryParams.push(options.maximum_price_per_night * 100)
+    if (queryString.includes('WHERE')) {
+      queryString += `
+   AND cost_per_night <= $${queryParams.length} 
+  `;
+    } else {
+      queryString += `WHERE cost_per_night <= $${queryParams.length}`
+    }
+  }
+   if (options.minimum_rating) {
+    queryParams.push(options.minimum_rating)
+    if (queryString.includes('WHERE')) {
+      queryString += `
+     AND rating >= $${queryParams.length} 
+    `;
+    } else {
+      queryString += `WHERE rating >= $${queryParams.length}`
+    }
+   }
 
   queryParams.push(limit);
   queryString += `
+  GROUP BY properties.id
   ORDER BY cost_per_night
   LIMIT $${queryParams.length};
   `;
